0a856e9401cfed63a9f2f69431d005922a70c59b
ranshaofan <ranshaofan1009@163.com>
2024-10-17 18:41:27 +0800

    all

--- js/common.js
+++ js/common.js
@@ -23,3 +23,43 @@ function decryptDes(ciphertext, key) {
     });
     return decrypted.toString(CryptoJS.enc.Utf8);
 }
+function ajaxRequest(url, params, successCallback, errorCallback) {
+    // 创建一个唯一的回调函数名
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+
+    // 将参数对象转换为字符串形式
+    var paramString = '';
+    for (var key in params) {
+        if (params.hasOwnProperty(key)) {
+            paramString += key + '=' + encodeURIComponent(params[key]) + '&';
+        }
+    }
+    paramString = paramString.slice(0, -1); // 移除末尾的'&'
+
+    // 创建一个 script 元素
+    var script = document.createElement('script');
+
+    // 设置回调函数名
+    window[callbackName] = function(response) {
+        try {
+            successCallback(response);
+        } finally {
+            // 清理
+            script.parentNode.removeChild(script);
+            delete window[callbackName];
+        }
+    };
+
+    // 设置请求地址，包括回调函数名
+    script.src = url + '?' + paramString + '&callback=' + callbackName;
+
+    // 注册错误处理函数
+    script.onerror = function() {
+        errorCallback('请求失败');
+        // 清理
+        delete window[callbackName];
+    };
+
+    // 将 script 元素添加到文档中
+    document.body.appendChild(script);
+}
